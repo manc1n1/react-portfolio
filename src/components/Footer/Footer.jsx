2efb0d54ec41e1dcc6be1483b8f8ed8a5273aed2
manc1n1 <mancinij1111@gmail.com>
2023-11-14 09:09:28 -0500

    Update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import { FaGithub, FaLinkedin, FaStackOverflow } from 'react-icons/fa6';
 
 export default function Footer() {
 	return (
-		<footer className="flex items-center justify-center sticky bottom-0 py-3 bg-slate-200 dark:bg-slate-950">
-			<h1 className="flex-1 ml-16 bg-clip-text text-transparent transition-all duration-500 bg-gradient-to-r to-green-800 via-green-500 from-green-400 bg-size-200 hover:bg-right font-bold text-xs sm:text-lg text-center">
+		<footer className="flex items-center justify-center flex-col sticky bottom-0 py-3 bg-slate-200 dark:bg-slate-950">
+			<h1 className="flex-1 bg-clip-text text-transparent transition-all duration-500 bg-gradient-to-r to-green-800 via-green-500 from-green-400 bg-size-200 hover:bg-right font-bold text-xs sm:text-lg text-center">
 				Made with{' '}
 				<div className="inline-block">
 					<h2>♥</h2>
@@ -23,7 +23,7 @@ export default function Footer() {
 					<h2>Falador</h2>
 				</a>
 			</h1>
-			<div className="flex space-x-2 text-xs sm:text-lg text-green-500">
+			<div className="flex space-x-2 text-xs sm:text-lg text-green-500 pt-1">
 				<a
 					href="https://github.com/manc1n1"
 					target="_blank"
